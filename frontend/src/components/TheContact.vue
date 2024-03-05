d288639f7656ca5cc7a89febe11bc85e664123f9
Szabo Levente <szabo.levente.dev@gmail.com>
2024-03-05 22:20:51 +0100

    contact us ui without backend communication

---
+++ frontend/src/components/TheContact.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const email = ref<string>()
+const categories = ref<string[]>(['Sponsorship Opportunities', 'User Feedback and Suggestions','Career Opportunities','General Inquiries'])
+const category=ref<string>()
+const message = ref<string>()
+
+const sendForm = () => {
+  console.log({email: email.value, category: category.value,message: message.value})
+}
+</script>
+<template>
+  <VContainer id="contact">
+    <VForm class="form">
+      <VCardTitle class="title">Contact us</VCardTitle>
+      <VTextField label="Email"
+      v-model="email" 
+      outlined/>
+      <VSelect label="Category of interest"
+      v-model="category" 
+      :items="categories" 
+      />
+      <VTextarea label="Message"
+      v-model="message"/>
+      <VBtn type="button" class="button" @click="sendForm">Send message</VBtn>
+    </VForm>
+  </VContainer>
+</template>
+<style scoped>
+.title {
+  text-align: center;
+}
+
+.form {
+  border: 1px solid var(--c-grey);
+  border-radius: 1rem;
+  padding: 2rem;
+  background-color: var(--c-light-blue)
+}
+
+.button {
+  display: flex;
+  align-items: center;
+}
+</style>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,22 +1,24 @@
 <script setup lang="ts">
 import TheHero from '@/components/TheHero.vue'
-import TheIntroduction from '@/components/TheIntroduction.vue';
-import TheServices from '@/components/TheServices.vue';
+import TheIntroduction from '@/components/TheIntroduction.vue'
+import TheServices from '@/components/TheServices.vue'
+import TheContact from '@/components/TheContact.vue'
 </script>
 
 <template>
-    <VContainer class="page">
-        <TheHero />
-        <TheIntroduction/>
-        <TheServices/>
-    </VContainer>
+  <VContainer class="page">
+    <TheHero />
+    <TheIntroduction />
+    <TheServices />
+    <TheContact />
+  </VContainer>
 </template>
 <style scoped>
 .page {
-    margin: 0;
-    width: 100%;
-    background-color: var(--c-light-black);
-    color: var(--c-orange);
+  margin: 0;
+  width: 100%;
+  background-color: var(--c-light-black);
+  color: var(--c-orange);
 }
 
 @media (min-width: 960px) {
