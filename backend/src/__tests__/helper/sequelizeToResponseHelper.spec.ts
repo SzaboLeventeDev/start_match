1a4a8dd6d03f75d393257d7c98fd9bc5455f396e
Szabo Levente <szabo.levente.dev@gmail.com>
2024-04-14 21:18:42 +0200

    fix test cases where sequelize record transformed with helper method

--- backend/src/__tests__/helper/sequelizeToResponseHelper.spec.ts
+++ backend/src/__tests__/helper/sequelizeToResponseHelper.spec.ts
@@ -1,11 +1,11 @@
-import { mockFAQRecords, mockFAQs } from "../../__mocks__/faq/mockFAQ";
-import sequelizeToResponseArrayHelper from "../../helper/sequelizeToResponseHelper";
-import { QuestionAndAnswerAttributes } from "../../models/questionAndAnswer";
+import { mockFAQRecords, mockFAQs } from '../../__mocks__/faq/mockFAQ';
+import { sequelizeToResponseArrayHelper } from '../../helper/sequelizeToResponseHelper';
+import { QuestionAndAnswerAttributes } from '../../models/questionAndAnswer';
 
-describe("Generic function to cast the incoming data to the specified type", () =>{ 
-  test("The function casts the incoming data to the specified type successfully", () => {
-    const transformedData = sequelizeToResponseArrayHelper<QuestionAndAnswerAttributes>(mockFAQRecords)
+describe('Generic function to cast the incoming data to the specified type', () => {
+  test('The function casts the incoming data to the specified type successfully', () => {
+    const transformedData = sequelizeToResponseArrayHelper<QuestionAndAnswerAttributes>(mockFAQRecords);
 
-    expect(transformedData).toEqual(mockFAQs)
-  })
-})
+    expect(transformedData).toEqual(mockFAQs);
+  });
+});
