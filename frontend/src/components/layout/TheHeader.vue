06bc38aed1d4a148ebdf5743e9627cb4ab012dbc
Szabo Levente <szabo.levente.dev@gmail.com>
2024-06-30 21:43:49 +0200

    refactoring TheHeader component

--- frontend/src/components/layout/TheHeader.vue
+++ frontend/src/components/layout/TheHeader.vue
@@ -1,31 +1,31 @@
 <script setup lang="ts">
-import { ref, computed } from 'vue';
-import NavButton from '../ui/NavButton.vue';
-import useAuthenticationStore from '@/stores/useAuthenticationStore';
-import router from '@/router';
+import { ref, computed } from 'vue'
+import NavButton from '../ui/NavButton.vue'
+import useAuthenticationStore from '@/stores/useAuthenticationStore'
+import router from '@/router'
 
-const isMobileNavBarVisible = ref(false);
-const authStore = useAuthenticationStore();
-const { logoutUser } = authStore;
+const isMobileNavBarVisible = ref(false)
+const authStore = useAuthenticationStore()
+const { logoutUser } = authStore
 
-const userId = computed(() => authStore.getUserId);
-const isLoggedIn = computed(() => authStore.getIsLoggedIn);
+const userId = computed(() => authStore.getUserId)
+const isLoggedIn = computed(() => authStore.getIsLoggedIn)
 const toggleMobileNav = () => {
-  isMobileNavBarVisible.value = !isMobileNavBarVisible.value;
+  isMobileNavBarVisible.value = !isMobileNavBarVisible.value
 }
 
 const handleLoginOrLogout = () => {
   if (isLoggedIn) {
-    logoutUser();
-    router.push({ name: 'home' });
-    return;
+    logoutUser()
+    router.push({ name: 'home' })
+    return
   }
-  router.push({ name: 'login' });
-  return;
+  router.push({ name: 'login' })
+  return
 }
 </script>
 <template>
-  <v-container class="header">
+  <v-container class="container flex justify-between absolute z-3">
     <router-link :to="{ name: isLoggedIn ? 'dashboard' : 'home' }">
       <v-label>SmartMatch</v-label>
     </router-link>
@@ -33,36 +33,42 @@ const handleLoginOrLogout = () => {
       icon="mdi-menu"
       color="var(--c-white)"
       size="large"
-      class="hamburger"
+      class="z-10 md:max-w-none lg:invisible"
       @click="toggleMobileNav"
     />
-    <v-container class="navbar">
-      <nav-button>About us</nav-button>
-      <nav-button>Our mission</nav-button>
-      <nav-button>Contact</nav-button>
-      <nav-button>Q&A</nav-button>
-      <router-link
-        v-if="isLoggedIn"
-        :to="{
-          name: 'profile',
-          params: {
-            userId: userId
-          }
-        }"
+      <v-container
+        class="hidden invisible lg:flex lg:justify-end lg:visible lg:m-0 lg:p-0 lg:w-auto"
       >
-        <nav-button>Profile</nav-button>
-      </router-link>
-      <router-link :to="{ name: isLoggedIn ? 'home' : 'login' }">
-      <nav-button @click="handleLoginOrLogout">
-        <template #icon>
-          <v-icon :icon="isLoggedIn ? 'mdi-logout' : 'mdi-login'" />
-        </template>
-        {{ isLoggedIn ? 'Logout' : 'Login' }}
-      </nav-button>
-    </router-link>
-    </v-container>
+        <nav-button>About us</nav-button>
+        <nav-button>Our mission</nav-button>
+        <nav-button>Contact</nav-button>
+        <nav-button>Q&A</nav-button>
+        <router-link
+          v-if="isLoggedIn"
+          :to="{
+            name: 'profile',
+            params: {
+              userId: userId
+            }
+          }"
+        >
+          <nav-button>Profile</nav-button>
+        </router-link>
+        <router-link :to="{ name: isLoggedIn ? 'home' : 'login' }">
+          <nav-button @click="handleLoginOrLogout">
+            <template #icon>
+              <v-icon :icon="isLoggedIn ? 'mdi-logout' : 'mdi-login'" />
+            </template>
+            {{ isLoggedIn ? 'Logout' : 'Login' }}
+          </nav-button>
+        </router-link>
+      </v-container>
   </v-container>
-  <v-container v-if="isMobileNavBarVisible" class="sidebar isVisible">
+  <v-container
+    :class="[
+      'right-0 flex flex-col bg-night-blue h-full py-20 pt-10 z-[2] absolute w-auto', isMobileNavBarVisible ? 'animate-slideIn': 'animate-slideOut'
+    ]"
+    >
     <nav-button v-if="!isLoggedIn">About us</nav-button>
     <nav-button v-if="!isLoggedIn">Our mission</nav-button>
     <nav-button>Contact</nav-button>
@@ -88,55 +94,3 @@ const handleLoginOrLogout = () => {
     </router-link>
   </v-container>
 </template>
-<style scoped>
-.header {
-  width: 100%;
-  display: flex;
-  justify-content: space-between;
-  position: absolute;
-  z-index: 3;
-}
-.navbar {
-  display: none;
-  visibility: hidden;
-  @media (min-width: 1024px) {
-    display: flex;
-    justify-content: flex-end;
-    visibility: visible;
-    margin: 0;
-    padding: 0;
-    width: auto;
-  }
-}
-
-.sidebar {
-  right: 0;
-  flex-direction: column;
-  opacity: 0;
-}
-
-.sidebar.isVisible {
-  opacity: 1;
-  transition: opacity 0.5s;
-  width: auto;
-  display: flex;
-  position: absolute;
-  background-color: var(--c-blue);
-  height: 100%;
-  padding: 5rem 0;
-  z-index: 2;
-}
-
-.hamburger {
-  z-index: 10;
-  @media (min-width: 1024px) {
-    display: none;
-  }
-}
-
-@media (min-width: 768px) {
-  .v-container {
-    max-width: none;
-  }
-}
-</style>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{js,ts,jsx,tsx,vue}'],
+  darkMode: 'selector',
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        'night-blue': '#14213d'
+      }
+    },
+    keyframes: {
+      slideIn: {
+        '0%': { transform: 'translateX(100%)', opacity: '0', width: '0' },
+        '100%': { transform: 'translateX(0)', opacity: '1', width: 'auto' }
+      },
+      slideOut: {
+        '0%': { transform: 'translateX(0)', opacity: '1', width: 'auto' },
+        '100%': { transform: 'translateX(100%)', opacity: '0', width: '0' }
+      }
+    },
+    animation: {
+      slideIn: 'slideIn 0.5s ease-in forwards',
+      slideOut: 'slideOut 0.5s ease-out forwards'
+    }
   },
-  plugins: [
-    autoprefixer: {}
-  ]
+  plugins: []
 }
