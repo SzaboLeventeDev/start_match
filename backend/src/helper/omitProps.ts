d8855cba8e77c0016769afe877132b088920823d
Szabo Levente <szabo.levente.dev@gmail.com>
2025-02-05 22:22:58 +0100

    add documentation for omitProps

--- backend/src/helper/omitProps.ts
+++ backend/src/helper/omitProps.ts
@@ -1,10 +1,14 @@
-const omitProps = <T extends object, K extends keyof T>(
-  obj: T,
-  ...props: K[]
-): Omit<T, K> => {
+/**
+ * @function omitProps
+ * @description Removes specified key-value pairs from an object based on the provided property names.
+ * @param obj The original object from which specified properties will be omitted.
+ * @param props The property names that should be removed from the object.
+ * @returns A new object with the specified properties removed.
+ */
+const omitProps = <T extends object, K extends keyof T>(obj: T, ...props: K[]): Omit<T, K> => {
   const result = { ...obj };
-  props.forEach((prop) => delete result[prop]);
+  props.forEach(prop => delete result[prop]);
   return result;
 };
 
-export default omitProps
+export default omitProps;
